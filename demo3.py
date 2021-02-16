a78666ec62fb1405b1ed0d5c9a2efe1a82b32819
karankumar106 <Karan333kumar@>
2021-02-16 09:40:19 +0530

    Initial commit

---
+++ demo3.py
@@ -0,0 +1,21 @@
+# Fetching Rows from Database
+# fetchall()
+import mysql.connector
+conn = mysql.connector.connect(host="localhost", user="root", password="0000", database="klu_cse")
+if (conn):
+    print("Connection Successful")
+else:
+    print("Connection Failed")
+cur = conn.cursor()
+try:
+    cur.execute("select * from student")
+    result = cur.fetchall()
+    for i in result:
+        print(i)
+    conn.commit()
+    print("Record Inserted Successfully")
+except:
+    conn.rollback()
+print(cur.rowcount, "Records Inserted")
+cur.close()
+conn.close()
